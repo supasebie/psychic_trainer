e301bc2dfa1a034a6be3e20d5a41fcef47fcb6a4
Sebastian <supasebie@gmail.com>
2025-05-08 12:31:48 -0700

    feat: initial commit

---
+++ lib/model/course.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart' show Color;
+
+class Course {
+  final String title, description, iconSrc;
+  final Color color;
+
+  Course({
+    required this.title,
+    this.description =
+        'Enhance your nueral connection across the body with this simple meditation',
+    this.iconSrc = "assets/icons/ios.svg",
+    this.color = const Color(0xFF7553F6),
+  });
+}
+
+final List<Course> courses = [
+  Course(title: "Body Scan Meditation"),
+  Course(
+    title: "Cognition transition between senses ",
+    iconSrc: "assets/icons/code.svg",
+    color: const Color(0xFF80A4FF),
+  ),
+];
+
+final List<Course> recentCourses = [
+  Course(title: "State Machine"),
+  Course(
+    title: "Animated Menu",
+    color: const Color(0xFF9CC5FF),
+    iconSrc: "assets/icons/code.svg",
+  ),
+  Course(title: "Flutter with Rive"),
+  Course(
+    title: "Animated Menu",
+    color: const Color(0xFF9CC5FF),
+    iconSrc: "assets/icons/code.svg",
+  ),
+];
---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,120 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart' as rive;
+import 'components/animated_btn.dart';
+import 'components/sign_in_dialog.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late rive.RiveAnimationController _btnAnimationController;
+
+  bool showSignIn = false;
+
+  @override
+  void initState() {
+    _btnAnimationController = rive.OneShotAnimation("active", autoplay: false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            left: 100,
+            bottom: 100,
+            child: Image.asset("assets/backgrounds/spline.png"),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: const SizedBox(),
+            ),
+          ),
+          const rive.RiveAnimation.asset("assets/riveAssets/shapes.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          AnimatedPositioned(
+            top: showSignIn ? -50 : 0,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            duration: const Duration(milliseconds: 260),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    const SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Master Your Intuition & Focus",
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.w900,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            "Don't let your mind control you. Control your mind. \nPsychic trainer is an application that elucidates the process of learning intuition and focus.",
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 2),
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+
+                        Future.delayed(const Duration(milliseconds: 800), () {
+                          setState(() {
+                            showSignIn = true;
+                          });
+                          if (!context.mounted) return;
+                          showCustomDialog(context, onValue: (_) {});
+                          // showCustomDialog(
+                          //   context,
+                          //   onValue: (_) {
+                          //     setState(() {
+                          //       isShowSignInDialog = false;
+                          //     });
+                          //   },
+                          // );
+                        });
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Sign in to start your journey to the center of your mind. Find access to meditation and prayer practices that will help unlock your potential!",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
